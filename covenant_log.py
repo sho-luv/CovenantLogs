aaf8bbc728c9b2dc7917734a8c30751ac8c1754e
sho-luv <sho.luv@gmail.com>
2020-12-10 16:32:25 -0800

    added bunch of stuff

--- covenant_log.py
+++ covenant_log.py
@@ -6,6 +6,8 @@ import sys # used sys to have clean exits of the program
 
 YELLOW='\033[1;33m'
 WHITE='\033[1;37m'
+GREEN='\033[1;32m'
+DARKGRAY='\033[1;30m'
 LIGHTCYAN='\033[1;36m'
 NOCOLOR='\033[0m'
 
@@ -30,8 +32,40 @@ def print_info(sqlfile):
                 'FROM GruntCommands '\
                 'INNER JOIN AspNetUsers ON (GruntCommands.UserId = AspNetUsers.Id) '\
                 'GROUP BY AspNetUsers.UserName;')
-
             all_rows = c.fetchall()
+
+            # Aligning text and numbers with .format() t.ly/upn8
+            print("\n"+WHITE,"Operator's Tasks:",NOCOLOR)
+            for row in all_rows:
+                username, count = row
+                print(LIGHTCYAN+"\t{:<20s}{:>1s}{:>6d}".format(username,":",count)+NOCOLOR)
+
+            """ I guessed these by looking at covenant.db and Grunt.cs
+                Uninitialized,  0
+                Stage0,         1
+                Stage1,         2
+                Stage2,         3
+                Active,         4
+                Lost,           5
+                Exited,         6
+                Disconnected,   7
+                Hidden          8
+            """
+            c.execute('select status, count(status) from grunts group by status;')
+            all_rows = c.fetchall()
+            print("\n"+WHITE,"Grunt Information:",NOCOLOR)
+            for row in all_rows:
+                grunt_status, grunt_count = row
+                if grunt_status == 4: 
+                    print(LIGHTCYAN+"\t{:<20s}{:>1s}{:>6d}".format("Grunts (Active)",":",grunt_count)+NOCOLOR)
+                elif grunt_status == 5:
+                    print(LIGHTCYAN+"\t{:<20s}{:>1s}{:>6d}".format("Grunts (Lost)",":",grunt_count)+NOCOLOR)
+            print()
+            #print(LIGHTCYAN,"  Grunts by hostname:\tToDo",NOCOLOR)
+            #print(LIGHTCYAN,"  Grunts by username:\tToDo",NOCOLOR)
+            #print(LIGHTCYAN,"  Grunts by integrity:\tToDo",NOCOLOR)
+            
+            #print(YELLOW,"\t\m/ (o_O) \m/",NOCOLOR,"\n")
         except sqlite3.error as e:
             print("database error: %s" % e)
         except exception as e:
@@ -40,18 +74,6 @@ def print_info(sqlfile):
             print(e)
             print("unhandled exception in look function:", sys.exc_info()[0])
             exit()
-
-        print("\n"+WHITE,"Operator's Tasks:",NOCOLOR)
-        for row in all_rows:
-            username, count = row
-            print(LIGHTCYAN,"  ",username,":\t",count,NOCOLOR)
-
-        print("\n"+WHITE,"Grunt Information:",NOCOLOR)
-        print(LIGHTCYAN,"  Grunts by hostname:\tToDo",NOCOLOR)
-        print(LIGHTCYAN,"  Grunts by username:\tToDo",NOCOLOR)
-        print(LIGHTCYAN,"  Grunts by integrity:\tToDo",NOCOLOR)
-        
-        print("\n"+YELLOW,"    \m/ (o_O) \m/",NOCOLOR,"\n")
 
 
 def get_logs(sqlfile):
@@ -86,8 +108,9 @@ def get_logs(sqlfile):
                 start_time, end_time, command, output, hostname, destination, operator, log_time = row
                 destination = destination.replace('"', '').replace('[','').replace(']','')
                 if options.output is True:
-                    print(LIGHTCYAN+"Command: ",command,NOCOLOR)
-                    print(YELLOW+"Results: ", output,NOCOLOR)
+                    print(GREEN+"[+] "+LIGHTCYAN+operator,"command at",start_time,"completed at",end_time,NOCOLOR)
+                    print(GREEN+"[+] "+LIGHTCYAN+command+NOCOLOR)
+                    print(GREEN+"[+] "+LIGHTCYAN+"Results:\n"+YELLOW+output,NOCOLOR)
                 elif options.log is True:
                     print(start_time+",",end_time+",",command+",,,,",hostname+",",destination+",,",operator+",",log_time)
 
@@ -120,9 +143,10 @@ Y88..88P 888 d88P Y88b.       888 Y88..88P Y88b 888      X88
     
     parser = argparse.ArgumentParser(description='This program pareses covenant.db to output opt logs in R7 Format')
     parser.add_argument('db', action='store', metavar='covenant.db', help="Covenant database\n")
+    parser.add_argument('-n', '--nocolor',default=None,action='store_true',help="remove color from output\n")
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-l','--log', default=None, action='store_true', help='Redteam operator formated logs.')
-    group.add_argument('-o','--output', default=None, action='store_true', help='Results of Covenant commands all of them.')
+    group.add_argument('-o','--output', default=None, action='store_true', help='Results of Covenant Grunt Taskings input commands and resulting output.')
 
     if len(sys.argv)==1:
         print(LIGHTCYAN,banner,NOCOLOR)
@@ -132,6 +156,8 @@ Y88..88P 888 d88P Y88b.       888 Y88..88P Y88b 888      X88
     options = parser.parse_args()
 
     if options.db is not None:
+        if options.nocolor is not None:
+            YELLOW=GREEN=WHITE=LIGHTCYAN=NOCOLOR=''
         if options.log is not None or options.output is not None:
             get_logs(options.db)
         else:
